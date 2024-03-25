438fd9e623bb698f47a712ce002f051adea2c9f3
AJWalkerX <87567671+AJWalkerX@users.noreply.github.com>
2024-03-25 16:23:22 +0300

    03 assignment is done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,19 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = "";
+    let lengthOfString = string.length;
+    let stringToArray = new Array();
+    
+    for(let i = 0; i < lengthOfString; i++){
+        let char = string.charAt(i);
+        stringToArray.push(char);
+    }
+    
+    let reverseArray = stringToArray.reverse();
+   
+    for(let i = 0; i < lengthOfString; i++){
+        reversedString += reverseArray[i];
+    }
+    return reversedString
 };
 
 // Do not edit below this line
