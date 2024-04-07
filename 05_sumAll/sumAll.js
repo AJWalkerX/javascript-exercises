7c4313beadd5864faf9984592255e3ed3ed03687
AJWalkerX <87567671+AJWalkerX@users.noreply.github.com>
2024-04-07 12:04:08 +0300

    File 05 is done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -6,6 +6,12 @@ const sumAll = function(num1, num2) {
         num2 = num1
         num1 = jokerNumber
     }
+    else if(num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
+    else if(!Number.isInteger(num1) || !Number.isInteger(num2)){
+        return "ERROR";
+    }
     while(num1 <= num2){
         sum += num1;
         num1++
