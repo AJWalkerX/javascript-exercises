ef3ca674b216519862a67ed3e374cd2e6d9c2c29
AJWalkerX <87567671+AJWalkerX@users.noreply.github.com>
2024-04-07 10:30:37 +0300

    file 04 it's done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(Array, ...items) {
+    return Array.filter(x=> !items.includes(x));    
 };
 
 // Do not edit below this line
