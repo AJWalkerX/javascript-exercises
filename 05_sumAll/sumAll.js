c0e8079c950467e9957667d5b30eb88db4ea6cc9
AJWalkerX <87567671+AJWalkerX@users.noreply.github.com>
2024-04-07 11:19:54 +0300

    File 05 passed with first 2 test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let jokerNumber
+    if (num1 > num2){
+        jokerNumber = num2
+        num2 = num1
+        num1 = jokerNumber
+    }
+    else if(num1 < 0 )
+    while(num1 <= num2){
+        sum += num1;
+        num1++
+    }
+    return sum
 };
 
 // Do not edit below this line
